f90a173fa6ef38a42af0d7560a743e2d47275295
vv <victor.o.tomanik@gmail.com>
2020-10-05 00:27:59 -0300

    Adicionando select no server manager

--- code/server_pstg/config_server.py
+++ code/server_pstg/config_server.py
@@ -1,8 +1,9 @@
-from . import models_pstq as model
-from sqlalchemy import create_engine, MetaData, Table
-from sqlalchemy.sql import insert
 import os
+
 from dotenv import load_dotenv
+from sqlalchemy import MetaData, Table, create_engine
+from sqlalchemy.sql import insert, text 
+import models_pstq as model
 
 load_dotenv()
 
@@ -11,6 +12,7 @@ class ServerManager():
         # postgres+psycop2://<username>:<password>@<IP>:<Port>/<Database_name>
         self.DataBaseUri = os.environ['server_url']
         self.engine = create_engine(self.DataBaseUri)
+        self.conn = self.engine.connect()
 
     def InsertData(self,TableName,**kargs):
         """ Função para adicionar dados na tabela
@@ -24,19 +26,21 @@ class ServerManager():
         #Cria o insert statement
         ins = insert(tabela).values(**kargs)
         #Se conecta à engine e executa o statement
-        conn = self.engine.connect()
-        conn.execute(ins)
+        self.conn.execute(ins)
 
-    def SelectData(self,TableName):
-        """Função para conseguir as informações na tabela selecionada"""
-
-        metadata = MetaData(bind=self.engine)
-        TableName = tas
-
+    def SelectData(self, Query):
+        """Função para conseguir as informações na tabela selecionada
+            parametros{
+        """
+        s= text('SELECT * FROM "AcoesDiario"')
+        return self.conn.execute(s)
 
     def CreateTables(self):
         model.Table.metadata.create_all(self.engine)
 
 
 if __name__ == '__main__':
-    ServerManager().CreateTables()
+    #dado = {'id':0,'date':'2020/10/01','open':34.1,'high':70,'low':3.1,'close':40.1,'volume':300}
+    #ServerManager().InsertData('AcoesDiario',**dado)
+    for i in ServerManager().SelectData("SELECT * from AcoesDiario"):
+        print(i)
