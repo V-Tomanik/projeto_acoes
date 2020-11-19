958b73f0b97d61795f1a7ab0067b577c74f64012
vv <victor.o.tomanik@gmail.com>
2020-11-19 15:40:12 -0300

    fazendo graficos

--- code/flask/flask_server.py
+++ code/flask/flask_server.py
@@ -15,8 +15,9 @@ server = Flask(__name__)
 
 @server.route('/')
 def home():
-    empresas = ["""'ITSA4'""","""'ITUB4'""","""'WEGE3'""","""'SUZB3'""","""'EGIE3'""","""'FLRY3'"""]
-    linhas = [[pm.graph_daily(f"{empresa}"),pm.table_empresa(f"{empresa}")]
+    empresas = ['ITSA4','ITUB4','WEGE3','SUZB3','EGIE3','FLRY3']
+    linhas = [[pm.graph_daily_candle(f"{empresa}"),pm.graph_daily_stock_prevision(f"{empresa}"),
+             pm.table_empresa(f"{empresa}"),empresa]
             for empresa in empresas]
     return render_template('main.html',empresas = linhas)
 
